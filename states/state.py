3d96c4e2a6f16c1897ebe323044ddcb6e4e308d5
rozievich <oybekrozievich@gmail.com>
2024-10-31 19:46:29 +0500

    Update code by rozievich

--- states/state.py
+++ states/state.py
@@ -1,6 +1,15 @@
 from aiogram.fsm.state import State, StatesGroup
 
 
-# Reklama uchun javobgar state
+# Reklama uchun javobgar statelar
 class AdsState(StatesGroup):
     ads = State()
+
+
+# Kanallar uchun javobgar statelar
+class ChannelAddState(StatesGroup):
+    url = State()
+    user_id = State()
+
+class ChannelDeleteState(StatesGroup):
+    user_id = State()
